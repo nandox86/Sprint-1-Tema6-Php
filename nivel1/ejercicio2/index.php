3124da54481ec144dda1fe547ee4dbc2c7db8edc
Nandox86 <orlando.arias03@gmail.com>
2025-04-08 11:46:22 +0200

    mejoras para que pueda ser visto en consola

--- nivel1/ejercicio2/index.php
+++ nivel1/ejercicio2/index.php
@@ -2,47 +2,51 @@
 /*Haz un programa que utilice al menos un par de constantes mágicas. * 
  */
 
+ echo PHP_EOL . PHP_EOL;
 
  define("EJERCICIO2", "Ejercicio 2");
- echo "<h1>" . EJERCICIO2 . "</h1>";
+ echo "<h1>" . EJERCICIO2 . "</h1>" . PHP_EOL;
 
 // incluimos los archivos de las clases
 require_once 'ChildClass.php';
+echo PHP_EOL;
 
 // definimos una constante para el nombre del script actual usando una constante mágica
 define('SCRIPT_NAME', __FILE__);
 
 // mostramos el nombre del script actual
-echo "nombre del script actual: " . SCRIPT_NAME . "<br>";
+echo "nombre del script actual: " . SCRIPT_NAME . "<br>" . PHP_EOL;
 
 // mostramos la línea actual del código usando otra constante mágica
-echo "esta linea de codigo se encuentra en la linea: " . __LINE__ . "<br>";
+
+echo "esta linea de codigo se encuentra en la linea: " . __LINE__ . "<br>". PHP_EOL . PHP_EOL;
 
 // obtenemos un valor de una variable superglobal ($_GET) con validacion
 if (isset($_GET['name'])) {
     // almacenamos el valor del parámetro 'name' en una variable
-    $userName = htmlspecialchars($_GET['name']); // usamos htmlspecialchars para evitar inyecciones de codigo
-    echo "hola, " . $userName . "!<br>";
+    $userName = htmlspecialchars($_GET['name']); // usamos htmlspecialchars para evitar inyecciones de codigo Agrega;(?name=TuNombre)al final de la URL
+    echo "hola, " . $userName . "!<br>". PHP_EOL;
 } else {
-    // mostramos un mensaje si el parámetro 'name' no está presente
-    echo "no se proporciono ningun nombre en la url.<br>";
+    // mostramos un mensaje si el parámetro 'name' no está presente 
+    echo "no se proporciono ningun nombre en la url.<br>". PHP_EOL;
 }
+echo PHP_EOL;
 
 // creamos una instancia de la clase hija
 $instance1 = new ChildClass("este es el primer mensaje");
 
 // llamamos al método para mostrar el mensaje
 $instance1->displayMessage();
-
+echo PHP_EOL;
 // mostramos información del objeto usando el método mágico __toString
-echo $instance1;
+echo $instance1 .PHP_EOL;
 
 // creamos otra instancia de la clase hija
 $instance2 = new ChildClass("este es el segundo mensaje");
 
 // llamamos al método para mostrar el mensaje
 $instance2->displayMessage();
-
+echo PHP_EOL;
 // mostramos información del segundo objeto usando el método mágico __toString
-echo $instance2;
+echo $instance2. PHP_EOL;
 ?>
--- nivel1/ejercicio3/index.php
+++ nivel1/ejercicio3/index.php
@@ -3,52 +3,57 @@
 */
 declare(strict_types=1);
 
+echo PHP_EOL . PHP_EOL;
 
 define("EJERCICIO3", "Ejercicio 3");
- echo "<h1>" . EJERCICIO3 . "</h1>";
+ echo "<h1>" . EJERCICIO3 . "</h1>". PHP_EOL;
 // incluir la clase User
 require_once 'Usuario.php';
+echo PHP_EOL;
 
 try {
     // crear un nuevo usuario
     $user = new User("Nando", "Arias");
+    echo PHP_EOL;
 
     // establecer edad usando el metodo magico __set
     $user->age = 40;
+    echo PHP_EOL;
 
     // mostrar informacion usando diferentes metodos
-    echo "Usando getters normales:<br>";
-    echo "Nombre: " . $user->getName() . "<br>";
-    echo "Apellido: " . $user->getSurname() . "<br>";
-    echo "Edad: " . $user->getAge() . "<br>";
+    echo "Usando getters normales:<br>". PHP_EOL;
+    echo "Nombre: " . $user->getName() . "<br>". PHP_EOL;
+    echo "Apellido: " . $user->getSurname() . "<br>". PHP_EOL;
+    echo "Edad: " . $user->getAge() . "<br>". PHP_EOL;
 
-    echo "<br>Usando metodo magico __get:<br>";
-    echo "Edad: " . $user->age . "<br>";
+    echo "<br>Usando metodo magico __get:<br>". PHP_EOL;
+    echo "Edad: " . $user->age . "<br>". PHP_EOL;
 
-    echo "<br>Usando metodo magico __toString:<br>";
-    echo $user . "<br>";
+    echo "<br>Usando metodo magico __toString:<br>". PHP_EOL;
+    echo $user . "<br>". PHP_EOL;
 
-    echo "<br>Usando getFullInfo:<br>";
-    echo $user->getFullInfo() . "<br>";
+    echo "<br>Usando getFullInfo:<br>". PHP_EOL;
+    echo $user->getFullInfo() . "<br>". PHP_EOL;
 
     // probar validaciones
-    echo "<br>Probando validaciones:<br>";
+    echo "<br>Probando validaciones:<br>". PHP_EOL;
     
     // intentar establecer una edad invalida
     try {
         $user->age = 150;  // esto deberia fallar
     } catch (InvalidArgumentException $e) {
-        echo "Error al establecer edad: " . $e->getMessage() . "<br>";
+        echo "Error al establecer edad: " . $e->getMessage() . "<br>". PHP_EOL;
     }
 
     // intentar crear un usuario con nombre vacio
     try {
         $invalidUser = new User("", "Smith");
     } catch (InvalidArgumentException $e) {
-        echo "Error al crear usuario: " . $e->getMessage() . "<br>";
+        echo "Error al crear usuario: " . $e->getMessage() . "<br>". PHP_EOL;
     }
 
 } catch (Exception $e) {
     echo "Error general: " . $e->getMessage();
 }
+echo PHP_EOL;
 ?>
